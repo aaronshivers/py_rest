157fa802da2bc9c4020f68af956553858750941c
aaronshivers <aaron@moonmail.org>
2019-06-18 21:52:31 -0500

    setup Product Model

--- app.py
+++ app.py
@@ -5,6 +5,32 @@ import os
 
 # Initialize application
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLAlCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize Database
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
+
+# Product Class/Model
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), unique=True)
+  description = db.Column(db.String(200))
+  price = db.Column(db.Float)
+  qty = db.Column(db.Integer)
+
+  def __init__(self, name, description, price, qty):
+    self.name = name
+    self.description = description
+    self.price = price
+    self.qty = qty
+
 
 @app.route('/', methods=['GET'])
 def get():
