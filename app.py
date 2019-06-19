00b192442c6b79e8b76e972d9c38b967a132fd15
aaronshivers <aaron@moonmail.org>
2019-06-18 21:58:25 -0500

    fixed typo in db config

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
-app.config['SQLAlCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize Database
 db = SQLAlchemy(app)
