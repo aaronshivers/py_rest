e86ff091623d580f7691145da65956b175f3c1bc
aaronshivers <aaron@moonmail.org>
2019-06-18 22:30:23 -0500

    setup POST /products and GET /products

--- app.py
+++ app.py
@@ -41,9 +41,28 @@ class ProductSchema(ma.Schema):
 product_schema = ProductSchema(strict=True)
 products_schema = ProductSchema(many=True, strict=True)
 
-@app.route('/', methods=['GET'])
-def get():
-  return jsonify({ 'msg': 'Hello People' })
+# POST /products
+@app.route('/products', methods=['POST'])
+def add_product():
+  name = request.json['name']
+  description = request.json['description']
+  price = request.json['price']
+  qty = request.json['qty']
+
+  new_product = Product(name, description, price, qty)
+
+  db.session.add(new_product)
+  db.session.commit()
+
+  return product_schema.jsonify(new_product)
+
+# GET /products
+@app.route('/products', methods=['GET'])
+def get_products():
+  all_products = Product.query.all()
+  result = products_schema.dump(all_products)
+
+  return jsonify(result.data)
 
 # Run Server
 if __name__ == '__main__':
